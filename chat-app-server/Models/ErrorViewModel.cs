ec116da3c483862edbdf5b7c07dd658631bfd0ba
amiram-yss <amiram-yss@users.noreply.github.com>
2022-05-23 21:55:28 +0300

    Add project files.

---
+++ chat-app-server/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace chat_app_server.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
