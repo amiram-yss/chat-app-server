5b7566731880b5308de5264b368d48cb7e40d7d1
amiram-yss <amiram-yss@users.noreply.github.com>
2022-05-31 00:36:49 +0300

    starting point

---
+++ chat-app-web-api/Chat.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace chat_app_web_api
+{
+    public class Chat
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        /*[Required]
+        public List<Contact> contacts { get; set; } = new List<Contact>(2);*/
+    }
+}
