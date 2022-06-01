a336a3ecf035aecc56c24de7615968f08e52ed5a
amiram-yss <amiram-yss@users.noreply.github.com>
2022-06-01 23:41:37 +0300

    API part 3, get not implemented

--- chat-app-web-api/Controllers/ChatsController.cs
+++ chat-app-web-api/Controllers/ChatsController.cs
@@ -7,22 +7,41 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using chat_app_web_api;
 using chat_app_web_api.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using System.IdentityModel.Tokens.Jwt;
+using chat_app_web_api.Service;
 
 namespace chat_app_web_api.Controllers
 {
+    [Authorize]
     public class ChatsController : ControllerBase
     {
         private readonly chat_app_web_apiContext _context;
+        private IMessagesService _service;
+
+        private string GetConnectedContactId()
+        {
+            var token = this.HttpContext.GetTokenAsync("access_token");
+            if (token == null)
+                return null;
+            var handler = new JwtSecurityTokenHandler();
+            var jwtSecurityToken = handler.ReadJwtToken(token.Result);
+            return User.Claims.FirstOrDefault
+                (c => c.Type.Equals("UserId", StringComparison.OrdinalIgnoreCase)).Value;
+        }
 
         public ChatsController(chat_app_web_apiContext context)
         {
             _context = context;
+            _service = new WebApiDatabaseMessagesService(context);
         }
 
-        
-        private bool ChatExists(int id)
+        [HttpGet]
+        [Route("contacts/{id}/messages")]
+        public IEnumerable<Message> GetMessagesWith(string id)
         {
-          return (_context.Chat?.Any(e => e.id == id)).GetValueOrDefault();
+            return _service.GetMessagesWith(GetConnectedContactId(), id);
         }
     }
 }
--- chat-app-web-api/Controllers/MessagesController.cs
+++ chat-app-web-api/Controllers/MessagesController.cs
@@ -7,12 +7,15 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ChatAppWebApi;
 using chat_app_web_api.Data;
+using chat_app_web_api.Service;
+using Microsoft.AspNetCore.Authorization;
 
 namespace chat_app_web_api.Controllers
 {
     public class MessagesController : ControllerBase
     {
         private readonly chat_app_web_apiContext _context;
+        private IMessagesService _service;
 
         public MessagesController(chat_app_web_apiContext context)
         {
